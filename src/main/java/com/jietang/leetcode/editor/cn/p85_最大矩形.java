42d218a0aa3e569c36c65d7e462198c221f6579a
obu_jcfa008 <obu_jcfa008@iflytek.com>
2021-05-10 11:48:14 +0800

    style: p85

--- src/main/java/com/jietang/leetcode/editor/cn/p85_最大矩形.java
+++ src/main/java/com/jietang/leetcode/editor/cn/p85_最大矩形.java
@@ -55,48 +55,51 @@
 
 package com.jietang.leetcode.editor.cn;
 
-/*
-
-{
-                {'1', '0', '1', '0', '0'},
-                {'1', '0', '1', '1', '1'},
-                {'1', '1', '1', '1', '1'},
-                {'1', '0', '0', '1', '0'}}
-        {{'1', '0'}}
-
-
- */
 public class p85_最大矩形 {
     public static void main(String[] args) {
         Solution solution = new p85_最大矩形().new Solution();
         solution.maximalRectangle(new char[][]{
-                {'1', '0', '1'},
-                {'1', '0', '1'},
-                {'1', '1', '1'},
-                {'1', '0', '0'}});
+                {'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}});
+
+
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     /*
-        从右下往左上遍历，记录该点到右边、下边最大的边长
+
      */
     class Solution {
         public int maximalRectangle(char[][] matrix) {
-            int sideLength = Math.min(matrix.length, matrix[0].length);
-            for (int startindex = 0; startindex < sideLength; startindex++) {
-                int height = startindex < matrix[0].length - 1 ? startindex : matrix[0].length - 1;
-                int width = startindex < matrix.length - 1 ? startindex : matrix.length - 1;
-                for (int i = startindex; i <  matrix[0].length; i++) {
-                    System.out.println(height + "," + i + "=" + matrix[height][i]);
-                }
-                for (int i = startindex + 1; i < matrix.length; i++) {
-                    System.out.println(i + "," + width + "=" + matrix[i][width]);
-                }
-
+            if (matrix.length == 0 || matrix[0].length == 0) {
+                return 0;
             }
-
-            return 0;
+            int[][] ele_length = new int[matrix.length][matrix[0].length];
+            int result = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                int num1_length = 0;
+                for (int j = matrix[i].length - 1; j >= 0; j--) {
+                    if (matrix[i][j] == '1') {
+                        num1_length++;
+                        ele_length[i][j] = num1_length;
+                        int m = i;
+                        int min_width = num1_length;
+                        while (m >= 0) {
+                            min_width = Math.min(min_width, ele_length[m][j]);
+                            result = Math.max(min_width * (i - m + 1), result);
+                            m--;
+                        }
+                    } else {
+                        num1_length = 0;
+                        ele_length[i][j] = num1_length;
+                    }
+                }
+            }
+            return result;
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
